cf987595e904c55a4fbc3ce14ab7c6360db5e905
Facephi <developer@facephi.com>
2026-07-07 18:56:20 +0200

    New SPM Version: 2.9.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/AgoraIO/AgoraLite_iOS.git", .upToNextMinor(from: "4.5.3")),
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMinor(from: "4.0.8")),
-        .package(url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.8.1"))
+        .package(url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.9.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -36,7 +36,7 @@ let package = Package(
                           ]
         ),
         .binaryTarget(name: "videoRecordingComponent",
-        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/SDK/FPHISDKVideoRecordingComponent/2.8.1/videoRecordingComponent.zip",
-        checksum: "3456046b87ba52eebc392e07f1ec04ee62cc0dddad73fdd2b3184fd259dec537"),
+        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/SDK/FPHISDKVideoRecordingComponent/2.9.0/videoRecordingComponent.zip",
+        checksum: "c7da0b0bc7061193b31c7bfb3e173e11d6d576aa439b3ee2f29eae3b8a0431ec"),
     ]
 )
